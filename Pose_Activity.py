bfd8e67d1901ac1c361f8c04dc2e21a09295d61d
youjunjer <40359899+youjunjer@users.noreply.github.com>
2023-05-09 00:47:37 +0800

    Update Pose_Activity.py

--- Pose_Activity.py
+++ Pose_Activity.py
@@ -1,21 +1,17 @@
+
 import cv2
 import mediapipe as mp
 import numpy as np
 import math
 
+#使用mediapipe套件
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_selfie_segmentation = mp.solutions.selfie_segmentation
 mp_pose = mp.solutions.pose
-BG_COLOR = (192, 192, 192) # gray
-ExAngle=40
-ExStatus=False
-countEx=0
-pose = mp_pose.Pose(
-        static_image_mode=False,
-        model_complexity=2,
-        enable_segmentation=True,
-        min_detection_confidence=0.5)
+pose = mp_pose.Pose()
+
+#計算角度
 def FindAngleF(a,b,c):    
     ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
     if ang<0 :
@@ -24,69 +20,40 @@ def FindAngleF(a,b,c):
         ang=360-ang
     return ang
 
-def countExF(HandAngel): 
-  global ExStatus
-  if HandAngel<40 and ExStatus==False:
-    countEx=1
-    ExStatus=True
-  elif HandAngel>40 :
-    countEx=0
-    ExStatus=False
-  else:
-    countEx=0
-  return countEx
-
-
-# For webcam input:
+#開始讀取影像
 cap = cv2.VideoCapture(0)
-with mp_selfie_segmentation.SelfieSegmentation(model_selection=0) as selfie_segmentation:
-  with mp_pose.Pose(
-      min_detection_confidence=0.5,
-      min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-      success, image = cap.read()
-      if not success:
-        print("Ignoring empty camera frame.")
-        # If loading a video, use 'break' instead of 'continue'.
-        continue
-      imgH,imgW=image.shape[0],image.shape[1]
-      #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-      results = pose.process(image) #偵測身體
-      #左手軸3點->11,13,15
-      if (not results.pose_landmarks==None): #至少有一個身體
-        a=np.array([results.pose_landmarks.landmark[11].x*imgW,results.pose_landmarks.landmark[11].y*imgH])
-        b=np.array([results.pose_landmarks.landmark[13].x*imgW,results.pose_landmarks.landmark[13].y*imgH])
-        c=np.array([results.pose_landmarks.landmark[15].x*imgW,results.pose_landmarks.landmark[15].y*imgH])
-        #算出角度
-        HandAngle=FindAngleF(a,b,c)
-        #print(HandAngle)
-        #算出次數
+while cap.isOpened():
+  ret, frame = cap.read() #讀取鏡頭畫面    
+  frame=cv2.flip(frame,1) #翻轉：-1上下、0上下左右、1左右
+  imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #轉換色彩頻道    
+  h, w, c = frame.shape #取得螢幕長寬色彩
+  results = pose.process(imgRGB) #偵測身體
+  #左手軸3點->11,13,15
+  if (not results.pose_landmarks==None): #至少有一個身體
+    a=(results.pose_landmarks.landmark[11].x,results.pose_landmarks.landmark[11].y)
+    b=(results.pose_landmarks.landmark[13].x,results.pose_landmarks.landmark[13].y)
+    c=(results.pose_landmarks.landmark[15].x,results.pose_landmarks.landmark[15].y)
+    #算出角度
+    HandAngle=FindAngleF(a,b,c)
+    print(HandAngle)
 
-        countEx=countEx+countExF(HandAngle)
-        print("countEx=",countEx)
-        #畫出點位
-        mp_drawing.draw_landmarks(
-            image,
-            results.pose_landmarks, #點
-            mp_pose.POSE_CONNECTIONS, #連線
-            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
-          )
-        image=cv2.flip(image, 1)
-        x13,y13=round((1-results.pose_landmarks.landmark[13].x)*imgW),int(results.pose_landmarks.landmark[13].y*imgH)
-        if (x13<imgW and x13>0) and (y13<imgH and y13>0):
-          cv2.putText(image, str(round(HandAngle,2)) , (x13,y13), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 3)
-        cv2.putText(image,  str(countEx) , (30,40), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-        # 畫面切割
-        # image.flags.writeable = True
-        # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        # results = selfie_segmentation.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-        # condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
-        # bg_image = np.zeros(image.shape, dtype=np.uint8)
-        # bg_image[:] = BG_COLOR
-        # image = np.where(condition, image, bg_image)
-        # image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        #顯示結果      
-      cv2.imshow('MediaPipe Pose',image)
-      if cv2.waitKey(5) & 0xFF == 27:
-        break
+    #畫出點位
+    mp_drawing.draw_landmarks(
+        frame,
+        results.pose_landmarks, #點
+        mp_pose.POSE_CONNECTIONS, #連線
+        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
+      )    
+   
+    x13,y13=round((results.pose_landmarks.landmark[13].x)*w),int(results.pose_landmarks.landmark[13].y*h)
+    if (x13<w and x13>0) and (y13<h and y13>0):
+      cv2.putText(frame, str(round(HandAngle,2)) , (x13,y13), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 3)
+  cv2.imshow('Webcam',frame) #顯示畫面內容
+  key=cv2.waitKey(1) #等候使用者按鍵盤指令
+  if key==ord('a'):  #a拍照
+      cv2.imwrite('webcam.jpg',frame) #拍照
+  if key==ord('q'):  #q退出
+      break
 cap.release()
+cv2.destroyAllWindows()
+
